ddbd2b074a2318fc04c132be78da00db9a60d444
oobando-40725271 <40725271@live.napier.ac.uk>
2025-10-02 23:45:05 -0600

    update 10

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,4 +1,4 @@
-package com.napier.devops;
+package com.napier.sem;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
@@ -8,7 +8,8 @@ public class App
     public static void main(String[] args)
     {
 // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        //MongoClient mongoClient = new MongoClient("localhost", 27000);
+        MongoClient mongoClient = new MongoClient ("mongo-dbserver");
 // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
 // Get a collection from the database
